d1fb2ea2b059c4f7165250b490bf6c043d2d773b
viniciusandd <viniciusemanoelandrade@hotmail.com>
2019-10-26 18:48:30 -0300

    rota iniciar simplex criada; funcao preparar objetivo adicionada; funcao adicionar variaveis de folga adicionada

---
+++ app/controllers/simplex.py
@@ -0,0 +1,49 @@
+from flask import request, jsonify
+import numpy as np
+from app import app
+
+@app.route('/simplex/iniciar', methods=['POST'])
+def iniciar_simplex():
+    json = request.get_json()
+    
+    funcao_objetivo = json['funcao_objetivo'] # string
+    restricoes = json['restricoes'] # array de strings
+
+    equacao = ""
+    equacao += preparar_funcao_objetivo(funcao_objetivo, len(restricoes))
+    equacao += adicionar_variaveis_de_folga(restricoes)
+
+    print(equacao)
+    # tabela = np.fromstring(equacao, dtype=str, sep=' ')
+    # print(tabela)
+
+    return jsonify({"status":1})
+
+def preparar_funcao_objetivo(funcao_objetivo, len_restricoes):
+    list_valores = funcao_objetivo.split('+')    
+    for i in range(len(list_valores)):
+        list_valores[i] = '-%s' % list_valores[i]
+    variaveis_de_folga = ""
+    for i in range(len_restricoes):
+        variaveis_de_folga += ' +xF%s' % (i + 1)
+    return '%s%s =0\n' % (' '.join(list_valores), variaveis_de_folga)
+
+def adicionar_variaveis_de_folga(list_restricoes):
+    variaveis_de_folga = ""
+    for i in range(len(list_restricoes)):
+        variaveis_de_folga += ' +0xF%s' % (i + 1)
+
+    arr_variaveis_de_folga_original = variaveis_de_folga.split()
+
+    print(arr_variaveis_de_folga_original)
+
+    str_restricoes = ""
+    for i in range(len(list_restricoes)):
+        arr_variaveis_de_folga_copia = []
+        arr_variaveis_de_folga_copia = arr_variaveis_de_folga_original
+        arr_variaveis_de_folga_copia[i] = '+1xF%s' % (i + 1)
+        str_restricoes += '%s\n' % list_restricoes[i].replace(' <= ', ' '.join(arr_variaveis_de_folga_copia))
+    return str_restricoes
+
+def montar_tabela():
+    pass
