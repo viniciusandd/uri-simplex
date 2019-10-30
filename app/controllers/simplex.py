99e1805cbe7b0c6aa11134935b6234914c40e627
viniciusandd <viniciusemanoelandrade@hotmail.com>
2019-10-29 23:48:17 -0300

    reshape do array de strings sendo feito dinamicamente

--- app/controllers/simplex.py
+++ app/controllers/simplex.py
@@ -7,31 +7,24 @@ def iniciar_simplex():
     json = request.get_json()
     
     funcao_objetivo = json['funcao_objetivo'] # string
-    restricoes = json['restricoes'] # array de strings
+    restricoes      = json['restricoes']      # array de strings
 
     equacao = ""
     equacao += preparar_funcao_objetivo(funcao_objetivo, len(restricoes))
     equacao += adicionar_variaveis_de_folga_nas_restricoes(restricoes)
-
-    tabela = criar_tabela(equacao)
-    print(tabela) 
+    tabela  = criar_tabela(equacao, len(restricoes), 2)
+    print(tabela)
 
     return jsonify({"status":1})
 
 def preparar_funcao_objetivo(funcao_objetivo, len_restricoes):
-    list_valores = funcao_objetivo.split('+')    
+    list_valores = funcao_objetivo.split('+')   
+    print(list_valores) 
     for i in range(len(list_valores)):
         list_valores[i] = '-%s' % list_valores[i]
 
     variaveis_de_folga = criar_variaveis_de_folga(len_restricoes)
     return '%s%s0 ' % (' '.join(list_valores), variaveis_de_folga)
-
-def criar_variaveis_de_folga(len_restricoes):
-    variaveis_de_folga = ""
-    for i in range(len_restricoes):
-        variaveis_de_folga += ' +0xF%s' % (i + 1)
-    variaveis_de_folga += ' ='
-    return variaveis_de_folga
 
 def adicionar_variaveis_de_folga_nas_restricoes(list_restricoes):
     variaveis_de_folga = criar_variaveis_de_folga(len(list_restricoes))
@@ -42,5 +35,12 @@ def adicionar_variaveis_de_folga_nas_restricoes(list_restricoes):
         str_restricoes += '%s ' % list_restricoes[i].replace(' <= ', ' %s' % ' '.join(arr_variaveis_de_folga))
     return str_restricoes
 
-def criar_tabela(equacao):
-    return np.array(equacao.strip().split(' ')).reshape(4, 6)
+def criar_variaveis_de_folga(len_restricoes):
+    variaveis_de_folga = ""
+    for i in range(len_restricoes):
+        variaveis_de_folga += ' +0xF%s' % (i + 1)
+    variaveis_de_folga += ' ='
+    return variaveis_de_folga
+
+def criar_tabela(equacao, len_restricoes, len_variaveis_de_decisao):
+    return np.array(equacao.strip().split(' ')).reshape(len_restricoes+1, len_variaveis_de_decisao+len_restricoes+1)
