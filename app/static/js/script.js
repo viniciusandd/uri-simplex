c298c68fd61f52b2a6dbafdffe7f3b597ab39183
Vinicius Emanoel Andrade <viniciusemanoelandrade@hotmail.com>
2019-11-07 17:45:35 -0300

    criação da página principal (onde o usuário dará entrada nas variáveis) iniciada

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,3 +1,19 @@
-$('#btn').click(function() {
-    alert('deu certo');
+$('#btn-prosseguir').click(function(e) {
+    e.preventDefault();
+
+    $('#primeira-etapa').hide(500);
+    $('#segunda-etapa').show(500);
+});
+
+$('#btn-voltar').click(function(e) {
+    e.preventDefault();
+
+    $('#primeira-etapa').show(500);
+    $('#segunda-etapa').hide(500);
+});
+
+$('#btn-calcular').click(function(e) {
+    e.preventDefault();
+
+    alert('Em desenvolvimento');
 });
