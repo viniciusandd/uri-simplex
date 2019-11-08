e183a2a77d726d72387272685813d05a12b97f23
Vinicius Emanoel Andrade <viniciusemanoelandrade@hotmail.com>
2019-11-08 17:53:50 -0300

    unificando a função que cria o html da função e das restrições

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -5,7 +5,9 @@ $('#btn-prosseguir').click(function(e) {
     let qtd_restricoes = parseInt($('#txt-restricoes').val());
 
     if (qtd_variaveis_decisao > 0 && qtd_restricoes > 0) {        
-        criar_funcao(qtd_variaveis_decisao);
+        let campos = criar_html(qtd_variaveis_decisao, 1); 
+        $('#div-funcao').append(conteudo);
+
         $('#div-primeira-etapa').hide(500);
         $('#div-segunda-etapa').show(500);        
     } else {
@@ -13,28 +15,24 @@ $('#btn-prosseguir').click(function(e) {
     }
 });
 
-function criar_funcao(qtd) {
+function criar_html(qtd_variaveis_decisao, qtd_restricoes) {
     var conteudo = '';
-    conteudo = '<label>Qual sua função?</label>';
-    conteudo += '<form class="form-inline">';
-    conteudo += '<div class="form-group mb-2">';
-    conteudo += '<input type="text" class="form-control-plaintext">';
-    conteudo += '<span class="badge badge-dark">X1</span>';
-    conteudo += '</div>';
-
     for (i=0; i<qtd; i++) {
+        conteudo = '<label>Qual sua função?</label>';
+        conteudo += '<form class="form-inline">';
         conteudo += '<div class="form-group mb-2">';
         conteudo += '<input type="text" class="form-control-plaintext">';
         conteudo += '<span class="badge badge-dark">X1</span>';
         conteudo += '</div>';
-    }    
-    conteudo += '</form>';
 
-    $('#div-funcao').append(conteudo);
-}
-
-function criar_restricoes() {
-
+        for (i=0; i<qtd; i++) {
+            conteudo += '<div class="form-group mb-2">';
+            conteudo += '<input type="text" class="form-control-plaintext">';
+            conteudo += '<span class="badge badge-dark">X1</span>';
+            conteudo += '</div>';
+        }
+        conteudo += '</form>'; 
+    }  
 }
 
 $('#btn-voltar').click(function(e) {
